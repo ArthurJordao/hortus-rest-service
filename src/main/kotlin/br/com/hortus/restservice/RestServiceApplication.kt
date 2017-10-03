041ef322b45ed2f1f74b00f17dba1ad61675c052
ArthurJordao <arthurbernardijordao@gmail.com>
2017-10-03 11:44:28 -0300

    first commit

---
+++ src/main/kotlin/br/com/hortus/restservice/RestServiceApplication.kt
@@ -0,0 +1,11 @@
+package br.com.hortus.restservice
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class RestServiceApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(RestServiceApplication::class.java, *args)
+}
